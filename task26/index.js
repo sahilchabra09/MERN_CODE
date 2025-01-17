330995faef1a7cf12fc96ad63b6f6e8c40108f07
Sahil Chabra <sahilchabra1509@gmail.com>
2025-01-17 09:34:07 +0530

    added-comments-to-task26

--- task26/index.js
+++ task26/index.js
@@ -4,34 +4,37 @@ const fs = require("fs");
 const PORT = 2000;
 const HOSTNAME = "localhost";
 
+// Read HTML pages
 const home = fs.readFileSync("./index.html", "utf-8");
 const about = fs.readFileSync("./about.html", "utf-8");
 const contact = fs.readFileSync("./contact.html", "utf-8");
+
+// Read CSS
 const styles = fs.readFileSync("./styles.css", "utf-8");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
+    // Home Page
     res.writeHead(200, { "Content-Type": "text/html" });
-    return res.end(home);
-  } 
-  else if (req.url === "/about") {
+    res.end(home);
+  } else if (req.url === "/about") {
+    // About Page
     res.writeHead(200, { "Content-Type": "text/html" });
-    return res.end(about);
-  } 
-  else if (req.url === "/contact") {
+    res.end(about);
+  } else if (req.url === "/contact") {
+    // Contact Page
     res.writeHead(200, { "Content-Type": "text/html" });
-    return res.end(contact);
-  }
-  else if (req.url === "/styles.css") {
+    res.end(contact);
+  } else if (req.url === "/styles.css") {
     // Serve the CSS file
     res.writeHead(200, { "Content-Type": "text/css" });
-    res.end(styles);}
-  else {
+    res.end(styles);
+  } else {
+    // 404 - Not Found
     res.writeHead(404, { "Content-Type": "text/html" });
-    return res.end("<h1>404 - Page not found</h1>");
+    res.end("<h1>404 - Page not found</h1>");
   }
 });
-
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
